ea3eb584908f269b261eddb3cccf0539d3694cde
SandhyaPhuyal11 <sandyphuyel@gmail.com>
2025-11-07 12:20:17 +0545

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        String userName = "Sandhya";
+        int luckyNumber = 20;
+
+        Console.WriteLine($"Your username is {userName} and lucky number is your {luckyNumber}");
+    }
+}
