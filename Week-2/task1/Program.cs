4293f9cea8744cf7dbdec53697e256a99577a9a4
SandhyaPhuyal11 <sandyphuyel@gmail.com>
2025-11-12 22:02:42 +0545

    week3 assignment

---
+++ Week-2/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialize the variables
+            string userName = "Sandhya";
+            int luckyNumber = 15;
+
+            // Use string interpolation to display the message
+            Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
+        }
+    }
+}
---
+++ Week-3/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Workshop
+{
+    public class Program
+    {
+    
+         static void Main(string[] args)
+        {
+            NullOperations nop = new NullOperations();
+            nop.PerformNullChecks();
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+    }  
+    }
